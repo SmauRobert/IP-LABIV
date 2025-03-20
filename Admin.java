6b9b4bade3cc818219a1a13398fb1820736c6346
Smau Robert <robertgeorgesmau@gmail.com>
2025-03-20 17:08:30 +0200

    lalala

--- Admin.java
+++ Admin.java
@@ -1,4 +1,21 @@
+import java.util.List;
+
 public class Admin implements Person {
     private String name;
     private String email;
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public ActivityType getHobby() {
+        return null;
+    }
+
+    @Override
+    public List<Reservation> getReservationsList() {
+        return List.of();
+    }
 }
--- Test.java
+++ Test.java
@@ -1,5 +1,34 @@
+import java.util.*;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Creare activitati
+        Activity football = new Activity("Football", "Play football with friends", 50.0);
+        Activity swimming = new Activity("Swimming", "Swim in the Olympic pool", 30.0);
+
+        // Creare client
+        Client client = new Client("John Doe", "john.doe@example.com");
+        client.setHobby(ActivityType.FOOTBALL);
+
+        // Creare rezervari
+        Date startTime1 = new Date();
+        Date endTime1 = new Date(startTime1.getTime() + 3600000); // +1 ora
+        Reservation reservation1 = new Reservation(startTime1, endTime1, football);
+
+        Date startTime2 = new Date();
+        Date endTime2 = new Date(startTime2.getTime() + 7200000); // +2 ore
+        Reservation reservation2 = new Reservation(startTime2, endTime2, swimming);
+
+        // Adaugare rezervari la client
+        List<Reservation> reservations = new ArrayList<>();
+        reservations.add(reservation1);
+        reservations.add(reservation2);
+
+        System.out.println("Client: " + client.getName() + " (Email: " + client.getEmail() + ")");
+        System.out.println("Hobby: " + client.getHobby());
+        System.out.println("Reservations:");
+        for (Reservation res : reservations) {
+            System.out.println(" - Activity: " + res.getReservedActivity().getTitle() + ", Time: " + res.getStartTime() + " to " + res.getEndTime());
+        }
     }
 }
